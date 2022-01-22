64fa910f001e131043eb1dc1a423df1412f6970f
xmg <2474775945@qq.com>
2022-01-22 12:22:56 +0800

    新增editlabel

--- src/views/EditLable.vue
+++ src/views/EditLable.vue
@@ -5,11 +5,25 @@
 </template>
 
 <script lang="ts">
-import Layout from '@/components/Layout.vue'
-export default {
-  components: { Layout },
 
-}
+import Vue from "vue"
+import { Component } from "vue-property-decorator";
+import taglistmode from "../models/tagslistmode"
+@Component
+ export default class EditLable extends Vue{
+     created(){
+         const id=this.$route.params.id
+         taglistmode.fetch()
+         const tags =taglistmode.data
+         const tag =tags.filter(item =>item.id ===id)[0]
+         if(tag){
+             console.log(tag);
+             
+         }else{
+             this.$router.replace("/404")
+         }
+     }
+ }
 </script>
 
 <style>
